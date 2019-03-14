72e79ad37e157c91888fa091efc76dc82ae3eaf2
2encounter <incounter@163.com>
2019-03-14 22:48:44 +0800

    user Eric Date 201903142242

--- src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
-import static org.smart4j.chapter2.util.PropsUtil.LOGGER;
 
 public class CustomerService {
     private static final Logger LOGGER= LoggerFactory.getLogger(DatabaseHelper.class);
